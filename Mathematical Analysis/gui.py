492f21e714053581d8f9197491de2d984c56511d
Noha Atef <atefnoha13@gmail.com>
2023-05-19 09:58:02 +0300

    Final project

--- Mathematical Analysis/gui.py
+++ Mathematical Analysis/gui.py
@@ -10,7 +10,7 @@ import gaussianelimination as gep
 import gauss as ge
 import Lu as lu
 import cramer as cr
-window = tb.Window(themename='superhero')
+window = tb.Window(themename='numanalysis')
 window.title('Numerical Analysis')
 
 note = ttk.Notebook(window)
@@ -51,49 +51,58 @@ def root():
         if cb1.get()=='False position':
             iterations , x2 = fal.falsePosition(f.get(),x0.get(),x1.get(),e.get())
             for i in range(0,len(iterations)):
-                labeli = Label(tab1,text=iterations[i])
+                labeli = Label(tab1,text=iterations[i],font=(12))
                 labeli.grid(row=(8+i),column=3,padx=(0,15))
                 iterations_lable.append(labeli)
-            labelx = tb.Label(tab1,text=f"root {x2}",bootstyle="warning")
-            labelx.grid(row=(10+len(iterations)),column=3,padx=(0,15))
+            labelx = tb.Label(tab1,text=f"root {x2}",bootstyle="success",font=(12))
+            labelx.grid(row=(10+len(iterations)),column=3,padx=(0,15),pady=2.5)
             
         elif cb1.get()=='Newton Raphson':
-            iterations , xk = ne.newtonRaphson(f.get(),x0.get(),e.get(),nn.get())
+            try:
+                iterations , xk = ne.newtonRaphson(f.get(),x0.get(),e.get())
+            except:
+                iterations , xk = ne.newtonRaphson(f.get(),x0.get(),nn.get())
             for i in range(0,len(iterations)):
-                labeli = Label(tab1,text=iterations[i])
+                labeli = Label(tab1,text=iterations[i],font=(12))
                 labeli.grid(row=(8+i),column=3,padx=(0,15))
                 iterations_lable.append(labeli)
-            labelx = tb.Label(tab1,text=f"root {xk}",bootstyle="warning")
-            labelx.grid(row=(10+len(iterations)),column=3,padx=(0,15))
+            labelx = tb.Label(tab1,text=f"root {xk}",bootstyle="success",font=(12))
+            labelx.grid(row=(10+len(iterations)),column=3,padx=(0,15),pady=2.5)
             
         elif cb1.get()=='secant':
-            iterations , xs = se.secant(f.get(),x0.get(),x1.get(),e.get(),nn.get())
+            try:
+                iterations , xs = se.secant(f.get(),x0.get(),x1.get(),e.get())
+            except:
+                iterations , xs = se.secant(f.get(),x0.get(),x1.get(),nn.get())
             for i in range(0,len(iterations)):
-                labeli = Label(tab1,text=iterations[i])
+                labeli = Label(tab1,text=iterations[i],font=(12))
                 labeli.grid(row=(9+i),column=3,padx=(0,15))
                 iterations_lable.append(labeli)   
-            labelx = Label(tab1,text=f"root {xs}")
-            labelx.grid(row=(11+len(iterations)),column=3,padx=(0,15))
+            labelx = tb.Label(tab1,text=f"root {xs}",bootstyle="success",font=(12))
+            labelx.grid(row=(11+len(iterations)),column=3,padx=(0,15),pady=2.5)
             
         elif cb1.get()=='Bisection':
             iterations , x2 = bi.bisection(f.get(),x0.get(),x1.get(),e.get())
             for i in range(0,len(iterations)):
-                labeli = Label(tab1,text=iterations[i])
+                labeli = Label(tab1,text=iterations[i],font=(12))
                 labeli.grid(row=(8+i),column=3,padx=(0,15))
                 iterations_lable.append(labeli)
-            labelx = tb.Label(tab1,text=f"root {x2}",bootstyle="warning")
-            labelx.grid(row=(10+len(iterations)),column=3,padx=(0,15))
+            labelx = tb.Label(tab1,text=f"root {x2}",bootstyle="success",font=(12))
+            labelx.grid(row=(10+len(iterations)),column=3,padx=(0,15),pady=2.5)
             
         elif cb1.get()=='Simple fixed point':
-            iterations , x2 = fi.fixedPointIteration(f.get(),g.get(),x0.get(),e.get(),nn.get())
+            try:
+                iterations , x2 = fi.fixedPointIteration(f.get(),g.get(),x0.get(),e.get())
+            except:
+                iterations , x2 = fi.fixedPointIteration(f.get(),g.get(),x0.get(),nn.get())
             for i in range(0,len(iterations)):
-                labeli = Label(tab1,text=iterations[i])
-                labeli.grid(row=(8+i),column=3,padx=(0,15))
+                labeli = Label(tab1,text=iterations[i],font=(11))
+                labeli.grid(row=(8+i),column=3,padx=(0,15),pady=3)
                 iterations_lable.append(labeli)
-            labelx = tb.Label(tab1,text=f"root {x2}",bootstyle="warning")
-            labelx.grid(row=(10+len(iterations)),column=3,padx=(0,15))
+            labelx = tb.Label(tab1,text=f"root {x2}",bootstyle="success",font=(12))
+            labelx.grid(row=(10+len(iterations)),column=3,padx=(0,15),pady=2.5)
     except:
-        lab = tb.Label (tab1,text='Error',bootstyle="danger").grid(row=8,column=3,padx=(0,10),pady=10)   
+        messagebox.showerror("Error","Check your entries")   
     
 def selected(event):
     if cb1.get() == 'False position' or cb1.get() == 'Bisection':
@@ -140,7 +149,7 @@ def selected(event):
         nn.grid(row=6,column=3,padx=(0,10),pady=(1,0))
         x1.grid_forget()
         labelx1.grid_forget()
-options = ['False position','Newton Raphson','Bisection','Simple fixed point','secant']
+options = ['Bisection','False position','Newton Raphson','Simple fixed point','secant']
 
 clicked = StringVar()
 clicked.set('')
@@ -172,6 +181,10 @@ a = []
 xx = []
 iterappend = []
 b_label = []
+def delmat():
+    global myentries
+    [widget.grid_forget() for widget in myentries if isinstance(widget, Entry)]
+    
 def clear():
     [widget.delete(0, END) for widget in myentries if isinstance(widget, Entry)]
 
@@ -180,20 +193,27 @@ def clearLabel():
     [widget.destroy() for widget in iterappend if isinstance(widget, Label)]
     [widget.destroy() for widget in b_label if isinstance(widget, Label)]
 def createmat():
-    global n,brac
+    global n,b_label
+    delmat()
+    clearLabel()
+    
+    b_label=[]
     try:
         n = int(varn.get())
         for i in range(n):
-            brac = Label(tab2,text='[').grid(row=i+2,column=0,padx=(15,0))
-            
+            brac1 = Label(tab2,text='[')
+            brac1.grid(row=i+2,column=0,padx=(15,0))
+            b_label.append(brac1)
             for j in range(n+1):
                 myentry = Entry(tab2,width=10)
                 myentry.grid(row=i+2,column=j+1,padx=5,pady=5)
                 myentries.append(myentry)
-            brac = Label(tab2,text=']').grid(row=i+2,column=j+2)
+            brac2 = Label(tab2,text=']')
+            brac2.grid(row=i+2,column=j+2)
+            b_label.append(brac2)
     except:
         messagebox.showerror("Error","Error,Enter a valid no of integers")          
-varn = tb.Entry(tab2,bootstyle="warning")
+varn = tb.Entry(tab2)
 varn.grid(row=0,column=1,columnspan=4,pady=10)
 matbut = Button(tab2,text="Show matrix",command=createmat)
 matbut.grid(row=0,column=5,padx=10)
@@ -208,168 +228,154 @@ def solve():
     clearLabel()
     b_label = []
     iterappend = []
-    # clearLabel()
-    # label.destroy()
-    # label2.destroy()
-    # labelb.destroy()
-    # try:
-    for i in range(len(myentries)):
-        arr.append(int(str(myentries[i].get()))) 
-    for j in range(len(myentries)):
-        luarr.append(int(str(myentries[j].get())))
-        
-        
-    # for i in iterappend:
-    #     i.destroy()   
     
-    if(cb2.get()=='GE with partial pivoting'):
-        a,xx = gep.slice(arr)
-        
-        for i in a:
-            labelb = Label(tab2,text='[     ')
-            labelb.grid(row=8+o,column=p)
-            b_label.append(labelb)
-            # iterappend.append(labelb)
-            for j in i:
-                print(round(j,3), end = ' ')
-                q = round(j,3)
-                label = Label(tab2,text=(q))
-                label.grid(row=8+o,column=p+1)
-                iterappend.append(label)
-                p+=1
-            labelb1 = Label(tab2,text=']')
-            labelb1.grid(row=8+o,column=p+1)
-            b_label.append(labelb1)
-            # iterappend.append(labelb)
-            p=0
-            o+=1
-            print()
-        for i in range(len(xx)):
-            label2 = Label(tab2,text=('x%d= ' % (i+1)+str(int(xx[i]))))
-            label2.grid(row=15,column=1+i)
-            iterappend.append(label2)
-            print(xx[i])
-        o=0
-        p=0
-    elif(cb2.get()=='Gaussian elimination'):
-        a,xx=ge.slice(n,arr)
-        
-        for i in a:
-            labelb = Label(tab2,text='[     ')
-            labelb.grid(row=8+o,column=p)
-            b_label.append(labelb)
-            # iterappend.append(labelb)
-            for j in i:
-                print(round(j,3), end = ' ')
-                q = round(j,3)
-                label = Label(tab2,text=(q))
-                label.grid(row=8+o,column=p+1)
-                iterappend.append(label)
-                p+=1
-            labelb1 = Label(tab2,text=']')
-            labelb1.grid(row=8+o,column=p+1)
-            b_label.append(labelb1)
-            # iterappend.append(labelb)
-            p=0
-            o+=1
-            print()
-        for i in range(len(xx)):
-            label2 = Label(tab2,text=('x%d= ' % (i+1)+str(int(xx[i]))))
-            label2.grid(row=15,column=1+i)
-            iterappend.append(label2)
-            print(xx[i])
-        o=0
-        p=0
-    elif(cb2.get()=='LU decomposition'):
-        a,xx,lul=lu.luu(luarr)
-        print(a)
-        print(xx)
-        print(lul)
-        
-        label=Label(tab2,text='LU:')
-        label.grid(row=8,column=1)
-        b_label.append(label)
-        for i in a:
-            label1 = Label(tab2,text='[           ')
-            label1.grid(row=8+o,column=p+2)
-            b_label.append(label1)
-            for j in i:
-                print(round(j,3), end = ' ')
-                q = round(j,3)
-                labelq = Label(tab2,text=(q))
-                labelq.grid(row=8+o,column=p+2)
-                iterappend.append(labelq)
-                p+=1
-            label_2 = Label(tab2,text=']')
-            label_2.grid(row=8+o,column=p+2)
-            b_label.append(label_2)
+    try:
+        for i in range(len(myentries)):
+            arr.append(int(str(myentries[i].get())))
+        for j in range(len(myentries)):
+            luarr.append(int(str(myentries[j].get())))
+
+
+        if(cb2.get()=='GE with partial pivoting'):
+            a,xx = gep.slice(arr)
             
+            for i in a:
+                labelb = Label(tab2,text='[     ')
+                labelb.grid(row=8+o,column=p)
+                b_label.append(labelb)
+                # iterappend.append(labelb)
+                for j in i:
+                    print(round(j,3), end = ' ')
+                    q = round(j,3)
+                    label = Label(tab2,text=(q))
+                    label.grid(row=8+o,column=p+1)
+                    iterappend.append(label)
+                    p+=1
+                labelb1 = Label(tab2,text=']')
+                labelb1.grid(row=8+o,column=p+1)
+                b_label.append(labelb1)
+                # iterappend.append(labelb)
+                p=0
+                o+=1
+                print()
+            for i in range(len(xx)):
+                label2 = Label(tab2,text=('x%d= ' % (i+1)+str(int(xx[i]))))
+                label2.grid(row=15,column=1+i)
+                iterappend.append(label2)
+                print(xx[i])
+            o=0
             p=0
-            o+=1
-            print()
-        label3=Label(tab2,text='---------------------------------')
-        label3.grid(row=11,column=1,columnspan=5)
-        b_label.append(label3)
-        label4=Label(tab2,text='L:')
-        label4.grid(row=13,column=1)
-        b_label.append(label4)
-        for i in lul:
-            label5 = Label(tab2,text='[           ')
-            label5.grid(row=11+o,column=p+2)
-            b_label.append(label5)
-            for j in i:
-                print(round(j,3), end = ' ')
-                q = round(j,3)
-                labelq1 = Label(tab2,text=(q))
-                labelq1.grid(row=11+o,column=p+2)
-                iterappend.append(labelq1)
-                p+=1
-            label6 = Label(tab2,text=']')
-            label6.grid(row=11+o,column=p+2)
-            b_label.append(label6)
-            p=0
-            o+=1
-            print()
-        for i in range(len(xx)):
-            label2 = Label(tab2,text=('x%d= ' % (i+1)+str(round(xx[i],3))))
-            label2.grid(row=20,column=2+i)
-            iterappend.append(label2)
-            # iterappend.append(label2)
-            print(xx[i])
-        label7=Label(tab2,text='---------------------------------')
-        label7.grid(row=17,column=1,columnspan=5)
-        b_label.append(label7)
-        
-        o=0
-        p=0
-    elif(cb2.get()=='Cramer'):
-        a,xx=cr.cramer(n,arr)
-        for i in range(len(a)):
-            labela = Label(tab2,text='A%d= '%(o+1),font = (12))
-            labela.grid(row=8+o,column=p+2)
-            iterappend.append(labela)
-            labeli = Label(tab2,text=a[i],font = (12))
-            labeli.grid(row=8+o,column=p+3)
-            iterappend.append(labeli)
-            p=0
-            o+=1
-            print()
-        # for i in a:
-        #     label = Label(tab2,text='A%d= '%(o+1),font = (12)).grid(row=8+o,column=p+2)
-        #     label = Label(tab2,text=i,font = (12)).grid(row=8+o,column=p+3)
-        #     iterappend.append(label)
-        #     p=0
-        #     o+=1
-        #     print()
+        elif(cb2.get()=='Gaussian elimination'):
+            a,xx=ge.slice(n,arr)
             
-        for i in range(len(xx)):
-            label2 = Label(tab2,text=('x%d= ' % (i+1)+str(int(xx[i]))),font=(12))
-            label2.grid(row=20,column=2+i)
-            iterappend.append(label2)
-            print(xx[i])
-        o=p=0
-    # except:
-    #     messagebox.showerror("Error","Error,check your entries")
+            for i in a:
+                labelb = Label(tab2,text='[     ')
+                labelb.grid(row=8+o,column=p)
+                b_label.append(labelb)
+                # iterappend.append(labelb)
+                for j in i:
+                    print(round(j,3), end = ' ')
+                    q = round(j,3)
+                    label = Label(tab2,text=(q))
+                    label.grid(row=8+o,column=p+1)
+                    iterappend.append(label)
+                    p+=1
+                labelb1 = Label(tab2,text=']')
+                labelb1.grid(row=8+o,column=p+1)
+                b_label.append(labelb1)
+                # iterappend.append(labelb)
+                p=0
+                o+=1
+                print()
+            for i in range(len(xx)):
+                label2 = Label(tab2,text=('x%d= ' % (i+1)+str(int(xx[i]))))
+                label2.grid(row=15,column=1+i)
+                iterappend.append(label2)
+                print(xx[i])
+            o=0
+            p=0
+        elif(cb2.get()=='LU decomposition'):
+            a,xx,lul=lu.luu(luarr)
+            print(a)
+            print(xx)
+            print(lul)
+            
+            label=Label(tab2,text='LU:')
+            label.grid(row=8,column=1)
+            b_label.append(label)
+            for i in a:
+                label1 = Label(tab2,text='[           ')
+                label1.grid(row=8+o,column=p+2)
+                b_label.append(label1)
+                for j in i:
+                    print(round(j,3), end = ' ')
+                    q = round(j,3)
+                    labelq = Label(tab2,text=(q))
+                    labelq.grid(row=8+o,column=p+2)
+                    iterappend.append(labelq)
+                    p+=1
+                label_2 = Label(tab2,text=']')
+                label_2.grid(row=8+o,column=p+2)
+                b_label.append(label_2)
+                
+                p=0
+                o+=1
+                print()
+            label3=Label(tab2,text='---------------------------------')
+            label3.grid(row=11,column=1,columnspan=5)
+            b_label.append(label3)
+            label4=Label(tab2,text='L:')
+            label4.grid(row=13,column=1)
+            b_label.append(label4)
+            for i in lul:
+                label5 = Label(tab2,text='[           ')
+                label5.grid(row=11+o,column=p+2)
+                b_label.append(label5)
+                for j in i:
+                    print(round(j,3), end = ' ')
+                    q = round(j,3)
+                    labelq1 = Label(tab2,text=(q))
+                    labelq1.grid(row=11+o,column=p+2)
+                    iterappend.append(labelq1)
+                    p+=1
+                label6 = Label(tab2,text=']')
+                label6.grid(row=11+o,column=p+2)
+                b_label.append(label6)
+                p=0
+                o+=1
+                print()
+            for i in range(len(xx)):
+                label2 = Label(tab2,text=('x%d= ' % (i+1)+str(round(xx[i],3))))
+                label2.grid(row=20,column=2+i)
+                iterappend.append(label2)
+                print(xx[i])
+            label7=Label(tab2,text='---------------------------------')
+            label7.grid(row=17,column=1,columnspan=5)
+            b_label.append(label7)
+            
+            o=0
+            p=0
+        elif(cb2.get()=='Cramer'):
+            a,xx=cr.cramer(n,arr)
+            for i in range(len(a)):
+                labela = Label(tab2,text='A%d= '%(o+1),font = (12))
+                labela.grid(row=8+o,column=p+2)
+                iterappend.append(labela)
+                labeli = Label(tab2,text=a[i],font = (12))
+                labeli.grid(row=8+o,column=p+3)
+                iterappend.append(labeli)
+                p=0
+                o+=1
+                print()
+            
+            for i in range(len(xx)):
+                label2 = Label(tab2,text=('x%d= ' % (i+1)+str(int(xx[i]))),font=(12))
+                label2.grid(row=20,column=2+i)
+                iterappend.append(label2)
+                print(xx[i])
+            o=p=0
+    except:
+        messagebox.showerror("Error","Error,check your entries")
     
     arr.clear()
 
